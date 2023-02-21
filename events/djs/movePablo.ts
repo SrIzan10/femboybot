3c17a73e737b035872efef4f12a574c0dfa7f233
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2023-02-21 21:19:36 +0100

    fix: wait no

--- events/djs/movePablo.ts
+++ events/djs/movePablo.ts
@@ -5,7 +5,7 @@ export default eventModule({
 	type: EventType.Discord,
 	name: 'voiceStateUpdate',
 	execute: async (oldState: VoiceState, newState: VoiceState) => {
-		if (newState.member!.id !== '703974042700611634') return;
+		if (newState.member!.id !== '954042602615869520') return;
 
 		if (newState.channelId === '940619088550559745' && (await newState.client.channels.fetch('839200013618774028') as VoiceChannel).members.size >= 1) {
 			await newState.member?.voice.setChannel('839200013618774028')
