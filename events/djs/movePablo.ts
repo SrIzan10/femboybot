f9eedaff9eae143c7e50829015e2167d0dacde8e
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2023-02-21 21:16:02 +0100

    feat: some random moving system

--- events/djs/movePablo.ts
+++ events/djs/movePablo.ts
@@ -1,10 +1,14 @@
 import { EventType, eventModule } from "@sern/handler";
-import { VoiceState } from "discord.js";
+import { VoiceChannel, VoiceState } from "discord.js";
 
 export default eventModule({
 	type: EventType.Discord,
 	name: 'voiceStateUpdate',
 	execute: async (oldState: VoiceState, newState: VoiceState) => {
-		if (newState.member!.id !== '954042602615869520') return;
+		if (newState.member!.id !== '703974042700611634') return;
+
+		if (newState.channelId === '940619088550559745' && (await newState.client.channels.fetch('839200013618774028') as VoiceChannel).members.size >= 1) {
+			await newState.member?.voice.setChannel('839200013618774028')
+		}
 	},
 });
